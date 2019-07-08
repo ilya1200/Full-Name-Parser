5949a4adfa239cfeda60cbd38e8d6d0ca76f748f
Ilya Livshits <ilya1200@gmail.com>
2019-07-08 23:51:27 +0300

    Add UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullNameComponent } from './full-name/full-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/full-name/full-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-name',
+  templateUrl: './full-name.component.html',
+  styleUrls: ['./full-name.component.scss']
+})
+export class FullNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
